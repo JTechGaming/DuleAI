33b09e024b5b62dffadd629f109aa48ca9258976
JTechGaming <piepnieuweboer@gmail.com>
2025-09-10 09:39:23 +0200

    Switched to ortools instead of pytorch

--- src/model/run.py
+++ src/model/run.py
@@ -2,3 +2,11 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 import src.datastructure as ds
+
+from ortools.sat.python import cp_model
+
+class Run:
+    def __init__(self) -> None:
+        self.model = cp_model.CpModel()
+        self.solver = cp_model.CpSolver()
+        
